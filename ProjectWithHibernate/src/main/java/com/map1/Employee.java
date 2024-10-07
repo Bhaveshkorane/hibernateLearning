f8014d1f3e5519d9de82c241000745989cd05297
bhaveshkorane <bhavesh.korane@tekditechnologies.com>
2024-10-07 15:18:28 +0530

    created employee table and added many to many fileds into it

--- ProjectWithHibernate/src/main/java/com/map1/Employee.java
+++ ProjectWithHibernate/src/main/java/com/map1/Employee.java
@@ -1,5 +1,54 @@
 package com.map1;
 
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+@Entity
 public class Employee {
+	
+	@Id
+	private int eid;
+	private String name;
+	
+	@ManyToMany
+	@JoinTable(name="emp_learn")
+	private List<Project> projects;
+	
+	
+	public List<Project> getProjects() {
+		return projects;
+	}
+	public void setProjects(List<Project> projects) {
+		this.projects = projects;
+	}
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Employee(int eid, String name, List<Project> projects) {
+		super();
+		this.eid = eid;
+		this.name = name;
+		this.projects = projects;
+	}
+	public int getEid() {
+		return eid;
+	}
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 
 }
